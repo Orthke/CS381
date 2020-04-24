c7746ba7ef4650b88b214491b0eb8030abd5ebd7
Kelton <kelton@orth.org>
2020-04-24 14:15:17 -0700

    Part A

---
+++ Assignment2.hs
@@ -0,0 +1,33 @@
+import Prelude hiding (Num)
+
+type Num = Int
+type Name = String
+
+
+data Cmd = Pen Mode
+         | Moveto Pos Pos
+         | Define Name [Pars] Cmd
+         | Call Name [Name]
+  deriving (Eq,Show)
+
+
+data Mode = Down
+          | Up
+  deriving (Eq,Show)
+
+
+data Pos = Num
+          | Myname Name
+  deriving (Eq,Show)
+
+
+data Pars = Myname3 Name Pars
+          | Myname4 Name
+  deriving (Eq,Show)
+
+
+data Vals = Mynum Num Vals
+          | Myname2 Name
+  deriving (Eq,Show)
+
+
