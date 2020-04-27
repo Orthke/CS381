9f67d4d925a34951ac87fa10ad096f4ca241c039
Kelton <kelton@orth.org>
2020-04-27 14:06:26 -0700

    Part b complete

--- Assignment2.hs
+++ Assignment2.hs
@@ -35,7 +35,13 @@ data Vals = I1 Int Vals
 
 
 --  Part b 
---vector = Define "vector" (S1 "x1" (S1 "x2" (S1 "y1" (S2 "y2")))) [Pen Down, Moveto(x1,y1), Moveto(x2,y2), Pen Up]
+
+x1 = I 0
+x2 = I 0
+y1 = I 0
+y2 = I 0
+
+vector = Define "vector" (S1 "x1" (S1 "x2" (S1 "y1" (S2 "y2")))) [Pen Down, Moveto(x1,y1), Moveto(x2,y2), Pen Up]
 
 
 --  Part c 
@@ -56,21 +62,9 @@ stepsHelper n = [Moveto(I (n-1), I n), Moveto(I (n-1), I (n-1))] ++ stepsHelper(
 
 --  Part a 
 
-type Circuit = (Gates, Links)
-
-type Gates = [(Int, GateFn)]
-data GateFn = And | Or | Xor | Not deriving Show
-
-type Links = [Link]
-data Link  = L (Int, Int) (Int, Int) deriving Show
-
+type Circuit = ((Int,Gate), [Link])
+data Gate = And | Or | Xor | Not
+data Link  = L (Int, Int) (Int, Int)
 
 
 --  Part b
-
-halfAdder :: Circuit
-halfAdder = ([(1, Xor), (2, And)], [L (1,1) (2,1), L ( 1,2) (2,2)])
-
-
---  Part c
-
