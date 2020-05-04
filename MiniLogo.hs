3ad643cab9c84b68720f953a1741fa0e3f317831
Carl-Bohme <bohmec@oregonstate.edu>
2020-05-04 02:27:48 -0700

    Update MiniLogo.hs

--- MiniLogo.hs
+++ MiniLogo.hs
@@ -18,5 +18,8 @@ semS :: Cmd -> State -> (State,Lines)
 semS (Pen m1) (m2,i1,i2)        = ((m1,i1,i2),[])
 semS (MoveTo i1 i2) (m1,i3,i4)  | m1 == Down    = ((m1,i1,i2),[(i3,i4,i1,i2)])
                                 | otherwise     = ((m1,i1,i2),[])
+--semS (Seq c1 c2) (m1,i3,i4)     = semS c1 (m1,i3,i4)
  
 --semS (c:cs) (m, x, y) = ((m,x,y),([x]))
+
+--sem' :: Cmd -> Lines
