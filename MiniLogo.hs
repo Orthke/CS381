69fde5ec2b75f4370b51632748800c02e9bf6dc9
Carl-Bohme <bohmec@oregonstate.edu>
2020-05-03 19:30:43 -0700

    First Changes (Not Functional)

--- MiniLogo.hs
+++ MiniLogo.hs
@@ -12,5 +12,9 @@ type State = (Mode,Int,Int)
 type Line  = (Int,Int,Int,Int)
 type Lines = [Line]
 
---semS :: Cmd -> State -> (State,Lines)
+semS :: Cmd -> State -> (State,Lines)
+semS (Pen m1) (m2,i1,i2)        = ((m1,i1,i2),[])
+semS (MoveTo i1 i2) (m1,i3,i4)  | m1 == Down    = ((m1,i1,i2),[(i3,i4,i1,i2)])
+                                | otherwise     = ((m1,i1,i2),[])
+ 
 --semS (c:cs) (m, x, y) = ((m,x,y),([x]))
